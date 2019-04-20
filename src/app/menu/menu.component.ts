3be83da8b0239cf7b60cfbd926d124953e3141ba
danabeled <danabeled@gmail.com>
2019-04-20 13:14:33 -0400

    make progress toward showing pictures

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,11 +10,16 @@ export class MenuComponent implements OnInit {
 
   constructor(private MenuService: MenuService){}
   menus;
-
+  img;
   ngOnInit() {
     this.MenuService.getTrucksWithMenus().subscribe((result) =>
     {
       this.menus = result;
+      console.log(this.menus);
+      for(let i=0; i<this.menus.length; i++)
+      {
+        this.img = btoa(String.fromCharCode.apply(null, this.menus[0].img.data.data));
+      }
     });
   }
 
